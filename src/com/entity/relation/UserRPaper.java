4a1b7c998c80df09cccfd37444be23391e8ce194
killrent <15jhfeng@stu.edu.cn>
2017-06-11 23:03:55 +0800

    Add files via upload

--- src/com/entity/relation/UserRPaper.java
+++ src/com/entity/relation/UserRPaper.java
@@ -1,13 +1,33 @@
 package com.entity.relation;
 
+import com.utills.JDBCUtils.SQLColumn;
+import com.utills.JDBCUtils.SQLIgnore;
+
+import java.util.Date;
+
+/**
+ * Created by 10388 on 2017/6/5.
+ */
 public class UserRPaper {
 
     private int id;
+
+    @SQLColumn("who")
     private int userId;
+
+    @SQLColumn("which")
     private int paperId;
+
+    @SQLColumn("submit_time")
     private String time;
-    private float accuracy;
+
+    @SQLColumn("sec_used")
     private int timeUsed;
+
+    private float accuracy;
+
+    public UserRPaper() {
+    }
 
     public int getId() {
         return id;
@@ -33,12 +53,22 @@ public class UserRPaper {
         this.paperId = paperId;
     }
 
+    public void setTime(String time) {
+        this.time = time;
+    }
+
     public String getTime() {
+
         return time;
     }
 
-    public void setTime(String time) {
-        this.time = time;
+    public void setTimeUsed(int timeUsed) {
+        this.timeUsed = timeUsed;
+    }
+
+    public int getTimeUsed() {
+
+        return timeUsed;
     }
 
     public float getAccuracy() {
