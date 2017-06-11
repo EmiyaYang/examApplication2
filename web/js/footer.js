5c56c50ff5d0ef1e2d0539b86a0de5e56aefab21
killrent <15jhfeng@stu.edu.cn>
2017-06-11 23:10:18 +0800

    Add files via upload

---
+++ web/js/footer.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ssHss on 2017/6/10.
+ */
+/**
+ * Created by Tnecesoc on 2017/6/10.
+ */
+
+$('head').append('<link rel="stylesheet" type="text/css" href="css/footer.css">');
+
+$('body').find('script:last').remove();
+
+$('body').append('<footer></footer>');
+
+$('footer').html('<div class="container"><div class="row"><p>Enimaxe - 在线考试系统</p><p>由<a href="#">@杨家淇</a>, <a href="#">@冯景恒</a>, 和 <a href="#">@谭达强</a>为提交 WEB 程序设计期末作业而制作. </p> <p class="mg-4 c-bg">嘴强考王系统小组 / 2017 / 春 </p></div></div>');
